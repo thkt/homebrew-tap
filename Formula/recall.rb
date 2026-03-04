9f8fd990ce50a8208e0a0f1c646d89c8182c79e9
Toshiaki Takahashi <36068828+thkt@users.noreply.github.com>
2026-03-04 16:19:04 +0900

    fix(recall): revert to correct sha256

--- Formula/recall.rb
+++ Formula/recall.rb
@@ -7,11 +7,11 @@ class Recall < Formula
   on_macos do
     on_arm do
       url "https://github.com/thkt/recall/releases/download/v0.1.0/recall-aarch64-apple-darwin.tar.gz"
-      sha256 "958ee1109307032ab4192ad23ccf41eb985625b2806422075d539868f6c8f1b0"
+      sha256 "62c00a6f4e43f3279073d4b0414cdab5a2882e911b720fbf6bb984e4446078ea"
     end
     on_intel do
       url "https://github.com/thkt/recall/releases/download/v0.1.0/recall-x86_64-apple-darwin.tar.gz"
-      sha256 "ccccdcb16c09013d0afd5cedd9a529caddb7b8fed45bc9f83d50b3f07915ee0e"
+      sha256 "98c04a44e45da1134be29998c1a106caa25ecefca9e64ca20b4275e16b944fd7"
     end
   end
 
